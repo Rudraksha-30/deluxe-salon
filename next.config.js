fd01560017bbd978e987a1f21e8e6c2b904da314
Rudraksha-30 <rudraksha.chouhan30@gmail.com>
2026-08-13 01:23:42 +0530

    Updated styles and content

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/your-repo-name",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
